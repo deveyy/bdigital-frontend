1f661084d9d591f28a781f8f256f3d31690d1e18
Tên của bạn <jungjihoonkr97@gmail.com>
2022-10-11 13:20:22 +0700

    implement client navbar and darkmode

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
-  return <div>Home</div>;
+function Home() {
+  const [loading, setLoading] = useState(false);
+  const [Homes, fetchHomes] = useState([]);
+
+  const getData = async () => {
+    try {
+      setLoading(true); // Set loading before sending API request
+      await fetch('https://bdigital-5o7iy.ondigitalocean.app/api/home/latest')
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          fetchHomes(res);
+          setLoading(false); // Stop loading
+        });
+    } catch (error) {
+      setLoading(false); // Stop loading
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {loading ? (
+          <>Loading..</>
+        ) : (
+          Homes.map((item, i) => {
+            return <li key={i}>{item.title}</li>;
+          })
+        )}
+      </ul>
+    </div>
+  );
 }
+export default Home;
